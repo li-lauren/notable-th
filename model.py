02322de91bbc26f4d8e4aa7b97ee6b31a93d6d12
li-lauren <laurenlidev@gmail.com>
2021-01-30 21:26:39 +0000

    Add Doct, Apt, and Patient Models

--- model.py
+++ model.py
@@ -3,7 +3,71 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from dataclasses import dataclass
 
+
 db = SQLAlchemy()
+
+@dataclass
+class Doctor(db.Model):
+    """A doctor."""
+    doc_id: int
+    fname: str
+    lname: str
+    
+    __tablename__ = "doctors"
+
+    doc_id = db.Column(db.Integer, 
+                        autoincrement=True,
+                        primary_key=True)
+
+    fname = db.Column(db.String(50), nullable=False)
+    lname = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<Doctor doc_id={self.doc_id} fname={self.fname}>'
+
+@dataclass
+class Patient(db.Model):
+    """A patient."""
+    pat_id: int
+    fname: str
+    lname: str
+    
+    __tablename__ = "patients"
+
+    pat_id = db.Column(db.Integer, 
+                        autoincrement=True,
+                        primary_key=True)
+
+    fname = db.Column(db.String(50), nullable=False)
+    lname = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<Patient pat_id={self.pat_id} fname={self.fname}>'
+
+
+@dataclass
+class Appointment(db.Model):
+    """An appointment."""
+    apt_id: int
+    doc_id: int
+    pat_id: int
+    date_time: datetime
+    kind: str
+
+    __tablename__ = "appointments"
+
+    apt_id = db.Column(db.Integer, 
+                        autoincrement=True,
+                        primary_key=True)
+    doc_id = db.Column(db.Integer, db.ForeignKey('doctors.doc_id'))
+    pat_id = db.Column(db.Integer, db.ForeignKey('patients.pat_id'))
+    date_time = db.Column(db.DateTime, nullable=False)
+    kind = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<Apt apt_id={self.apt_id} doc_id={self.doc_id} pat_id={self.pat_id}>'
+
+
 
 def connect_to_db(flask_app, db_uri='postgresql:///notable_db', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
